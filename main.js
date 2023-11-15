64bee2cd0efc18227f3fc2866b8dc1855f2fa5a8
YasuhikoNoto <notty.tng@gmail.com>
2023-11-15 22:01:07 +0900

    231115

--- main.js
+++ main.js
@@ -29,3 +29,53 @@ const PI = 3.14;
 console.log(PI);
 // PI = 3.1;と記述しても書き換えできない
 
+let myName = 'Noto';
+let num = 123;
+let dec = 123.456;
+console.log(myName);
+console.log(num);
+console.log(dec);
+console.log(typeof myName);
+console.log(typeof num);
+console.log(typeof dec);
+myName = 456;
+console.log(myName);
+console.log(typeof myName);
+//動的型付け言語　javascriptのように代入時にデータ型が決まる言語　一つの変数のデータ型を変えることはバグの原因となりうるため推奨されない
+//静的型付け言語　変数の宣言時にデータ型が決まる言語
+
+let ans = 1 + 2;
+console.log(ans);
+
+let i = 20;
+let j = 10;
+let ans1 = i + j;
+console.log(ans1);
+let ans2 = i - j;
+console.log(ans2);
+let ans3 = i * j;
+console.log(ans3);
+let ans4 = i / j;
+console.log(ans4);
+
+let x = 3;
+let y = 2;
+let ans5 = x / y;
+console.log(ans5);
+let ans6 = x % y;
+console.log(ans6);
+let ans7 = Math.pow(x, y);//べき乗 powはべき乗を表すpower
+console.log(ans7);
+let ans8 = x ** y;//**もべき乗
+console.log(ans8);
+let ans9 = x * x;
+console.log(ans9);
+
+//演算子の優先順位
+let l = 1;
+let m = 2;
+let n = 3;
+let ans10 = l + m * n;
+console.log(ans10);
+let ans11 = (l + m) * n;
+console.log(ans11);
